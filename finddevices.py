364f5cd3eabde80d5ea9c61709305b99c0700575
Karthi Keyan <70126510+keyansaravanan@users.noreply.github.com>
2021-12-01 08:28:24 +0530

    Update finddevices.py

--- finddevices.py
+++ finddevices.py
@@ -1,4 +1,11 @@
-from pymf import get_MF_devices
-device_list = get_MF_devices()
-for i, device_name in enumerate(device_list):
-    print(f"opencv_index: {i}, device_name: {device_name}")
+import cv2
+import numpy as np
+
+all_camera_idx_available = []
+
+for camera_idx in range(10):
+    cap = cv2.VideoCapture(camera_idx)
+    if cap.isOpened():
+        print(f'Camera index available: {camera_idx}')
+        all_camera_idx_available.append(camera_idx)
+        cap.release()

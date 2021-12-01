e6089c21a927196375adcde9daa6e1fc278b9adc
Karthi Keyan <70126510+keyansaravanan@users.noreply.github.com>
2021-12-01 08:43:46 +0530

    Update testingscript.py

--- testingscript.py
+++ testingscript.py
@@ -1,6 +1,7 @@
-import torch
-from pprint import pprint
-
-_, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
-                              model='silero_vad',
-                              force_reload=True)
+import pyaudio
+p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name')
